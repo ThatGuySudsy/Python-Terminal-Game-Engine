177fe1a23f9fe6c71901ed78f54464f10a072d61
ThatGuySudsy <sudsysuds716@gmail.com>
2026-04-09 09:50:32 -0700

    Implement game loop for rendering and movement
    Added a game loop to continuously render and update positions.

--- TerminalGameEngine/testGame.py
+++ TerminalGameEngine/testGame.py
@@ -1,6 +1,12 @@
 import entities as e
 import render as r
+import time as t
 
-e.Rect("Goomba", 3, 3, 2, 1)
+goomba = e.Rect("Goomba", "@", 50, 3, 2, 1)
+mario = e.Rect("Mario", "#", 60, 3, 2, 0)
 
-r.render()
+while True:
+    r.render()
+    goomba.x -= 1
+    mario.x -= 2
+    t.sleep(0.1)
